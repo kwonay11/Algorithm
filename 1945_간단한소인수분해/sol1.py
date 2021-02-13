e7940bbd9a19171daf4a32f11c5db836d39f31fc
kwon yebeen <50578895+kwonay11@users.noreply.github.com>
2021-02-13 15:10:03 +0900

    Add files via upload

---
+++ 1945_간단한소인수분해/sol1.py
@@ -0,0 +1,31 @@
+import sys
+sys.stdin = open("input.txt")
+
+T = int(input())
+
+for tc in range(1, T+1):
+    N = int(input())
+    a = 0
+    b = 0
+    c = 0
+    d = 0
+    e = 0
+    while N % 2 == 0:
+            a += 1
+            N /= 2
+
+    while N % 3 == 0:
+            b += 1
+            N /= 3
+    while N % 5 == 0:
+            c += 1
+            N /= 5
+    while N % 7 == 0:
+            d += 1
+            N /= 7
+    while N % 11 == 0:
+            e += 1
+            N /= 11
+
+    print("#{} {} {} {} {} {}".format(tc, a,b,c,d,e ))
+
