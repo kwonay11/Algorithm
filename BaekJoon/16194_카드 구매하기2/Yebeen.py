2217def5d47e3085c2b33c07c31582d696b6e59d
yebeen <kwonay11@naver.com>
2021-07-06 13:20:32 +0900

    dp

---
+++ BaekJoon/16194_카드 구매하기2/Yebeen.py
@@ -0,0 +1,8 @@
+N = int(input())
+dp = [0]+list(map(int,input().split()))
+#합이 N인것의 조합, dp[i]에 최솟값 갱신
+for i in range(N+1):
+    for j in range(1,i+1):
+        dp[i] = min(dp[i],dp[i-j]+dp[j])
+print(dp[N])
+# print(dp)
