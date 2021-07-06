f45e4119935ed04f0fda94b6b6a424ce18dc0a98
yebeen <kwonay11@naver.com>
2021-07-06 13:02:25 +0900

    dp

--- BaekJoon/11052_카드구매하기/sol.py
+++ BaekJoon/11052_카드구매하기/sol.py
@@ -1,23 +1,7 @@
-from itertools import combinations
 N = int(input())
-P = list(map(int,input().split()))
-res = []
-temp =[]
-nums = []
-temp2 =[]
-for i in range(N):
-    nums.append(i+1)
-print(nums)
-
-for i in range(N):
-    temp = []
-    temp = combinations(nums,i)
-    if sum(list(temp)) == N: #temp의 합이 N과 같다면 해당값-1이 P의 인덱스이므로
-        print(list(temp))
-        temp2 =[]
-        for j in temp:
-            temp2.append(P[i-1])
-        res.append(sum(temp2))
-    print(res)
-
-###아직 못품
+dp = [0]+list(map(int,input().split()))
+#합이 N인것의 조합, dp[i]에 최댓값 갱신
+for i in range(N+1):
+    for j in range(1,i+1):
+        dp[i] = max(dp[i],dp[i-j]+dp[j])
+print(dp[N])
