d3bf1c8302f842b4b4a60c1c832554ee176c57ba
yebeen <kwonay11@naver.com>
2021-07-19 08:12:57 +0900

    그리디

--- Programmers/체육복.py
+++ Programmers/체육복.py
@@ -1,12 +1,9 @@
 # 여벌 체육복을 가져온 학생이 체육복을 도난당했을 수 있다..
 # 그렇다면..reverse에서 lost와 같은것이 있으면 제거
 def solution(n, lost, reserve):
-    lost2 = lost
-    reserve2 = reserve
-    for a in lost:
-        if a in reserve:
-            reserve2.remove(a)
-            lost2.remove(a)
+    #set으로 중복(같은거) 제거
+    lost2 = list(set(lost) - set(reserve))
+    reserve2 = list(set(reserve) - set(lost))
 
     for i in reserve2:
         if i-1 in lost2:
@@ -16,8 +13,8 @@ def solution(n, lost, reserve):
             lost2.remove(i+1)
             continue
 
-    print(lost2)
-    print(reserve2)
+    # print(lost2)
+    # print(reserve2)
     answer = n - len(lost2)
     return answer
 
