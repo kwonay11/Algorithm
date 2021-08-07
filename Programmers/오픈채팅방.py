373d5ce6f1604cf1d6c9a7a783d2633d78aa1901
yebeen <kwonay11@naver.com>
2021-08-07 09:58:29 +0900

    딕셔너리

--- Programmers/오픈채팅방.py
+++ Programmers/오픈채팅방.py
@@ -1,25 +1,18 @@
 def solution(record):
-    #해시를 사용하여 Enter Change 일때 그에 맞는 닉네임으로 해시의 값을 바꾸면 됩니다
+    #딕셔너리를 사용하여 Enter Change 일때 그에 맞는 닉네임으로 해시의 값을 바꾸면 됩니다
     answer = []
-    res = []
+    res = {}
     for i in record:
         temp = i.split()
-        res.append(temp)
+        if temp[0] == 'Enter' or temp[0] =='Change':
+            res[temp[1]] = temp[2] #아이디를 키로 가지고있는 딕셔너리를 닉네임으로 바꾼다.
 
-    for i in range(len(res)):
-        for j in range(0, i):
-            if res[i][1] == res[j][1]:
-                if res[i][0] == 'Change' or res[i][0] == 'Enter':
-                    res[j][2] = res[i][2]
-                else: #Leave
-                    res[i].append(res[j][2])
-
-    for i in range(len(res)):
-        if res[i][0] == 'Enter':
-            answer.append(res[i][2]+"님이 들어왔습니다.")
-        elif res[i][0] == 'Leave':
-            answer.append(res[i][2]+"님이 나갔습니다.")
-
+    for i in record:
+        temp = i.split()
+        if temp[0] == 'Enter':
+            answer.append(res[temp[1]]+"님이 들어왔습니다.")
+        elif temp[0] =='Leave':
+            answer.append(res[temp[1]]+"님이 나갔습니다.")
 
     return answer
 
