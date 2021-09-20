a633d06ccb066836dd31ca3e5f1fb5d4d3c9591a
yebeen <kwonay11@naver.com>
2021-09-20 21:48:46 +0900

    스택

--- Programmers/다리를 지나는 트럭.py
+++ Programmers/다리를 지나는 트럭.py
@@ -1,25 +1,18 @@
 def solution(bridge_length, weight, truck_weights):
     answer = 0
     #스택 위에 올라가는거, 스택에서 빼는거 = 시간 +=1
-    stack = []
+    #스택에 들락날락 -> += 1
+    stack = [0]*bridge_length
     time = 0
-
-    for i in range(len(truck_weights)):
-        num = truck_weights.pop(0)
-        stack.append(num)
+    while stack:
         time += 1
-        if sum(stack) > weight:
-            num2 = stack.pop()
-            truck_weights.insert(0,num2)
-            time += (len(stack)+1)
-            stack = []
-
-
-        if len(stack) == bridge_length:
-            stack = []
-            time += bridge_length
-
-
+        stack.pop(0)
+        if truck_weights:
+            # 스택합과 남은트럭 맨 앞꺼와 더해서 weight 이하라면 뽑아서 스택에 넣는다
+            if sum(stack) + truck_weights[0] <= weight:
+                stack.append(truck_weights.pop(0))
+            else:#아니라면 0넣음
+                stack.append(0)
     return time
 
 print(solution(2,10,[7,4,5,6]))
